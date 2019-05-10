b9d55bc84723f5924b3c971c5613587d5ccda857
Zubair Amjad <amjadz@uw.edu>
2019-05-10 01:18:25 -0700

    Got all the models done

--- realestate/realestateapp/models.py
+++ realestate/realestateapp/models.py
@@ -10,7 +10,21 @@ class Neighborhood(models.Model):
 
 
 class Property_Photo(models.Model):
+    property_photo_id = models.AutoField(primary_key=True)
+    property_id = models.ForeignKey()
+    photo_id = models.ForeignKey()
+
+class Photo(models.Model):
     photo_id = models.AutoField(primary_key=True)
     photo_file = models.URLField()
     photo_added_date = models.DateTimeField()
 
+class Property_Amenity(models.Model):
+    property_amenity_id = models.AutoField(primary_key=True)
+    property_id = models.ForeignKey()
+    amenity = models.ForeignKey()
+
+class Amenity(models.Model):
+    amenity_id = models.AutoField(primary_key=True)
+    amenity_name = models.CharField(max_length=225)
+    amenity_desc = models.TextField()
