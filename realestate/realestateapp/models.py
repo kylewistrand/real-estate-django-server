a4801b6adef540e74434e86aa9f952ef90d21cc2
Zubair Amjad <amjadz@uw.edu>
2019-05-10 00:26:05 -0700

    Got neighborhood model done

--- realestate/realestateapp/models.py
+++ realestate/realestateapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Neighborhood(models.Model):
+    neighborhood_id = models.AutoField(primary_key=True)
+    neighborhood_name = models.CharField(max_length=225)
+    neighborhood_desc = models.TextField()
