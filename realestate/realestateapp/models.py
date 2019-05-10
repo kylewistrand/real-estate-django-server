1deccbfb11374d5b3b6c89d09f9aed9dac2ce064
Zubair Amjad <amjadz@uw.edu>
2019-05-10 00:32:48 -0700

    Added property photo model

--- realestate/realestateapp/models.py
+++ realestate/realestateapp/models.py
@@ -7,3 +7,10 @@ class Neighborhood(models.Model):
     neighborhood_id = models.AutoField(primary_key=True)
     neighborhood_name = models.CharField(max_length=225)
     neighborhood_desc = models.TextField()
+
+
+class Property_Photo(models.Model):
+    photo_id = models.AutoField(primary_key=True)
+    photo_file = models.URLField()
+    photo_added_date = models.DateTimeField()
+
