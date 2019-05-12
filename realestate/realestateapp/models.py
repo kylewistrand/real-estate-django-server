836fb49c5b75d0eb23434dc42712342046f2f8ec
joesyoon <joesyoon@uw.edu>
2019-05-11 20:00:26 -0700

    Merge models

--- realestate/realestateapp/models.py
+++ realestate/realestateapp/models.py
@@ -1,13 +1,44 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class CouponType(models.Model):
+    couponTypeName = models.CharField(max_length=30)
+    couponTypeDescription = models.CharField(max_length=250)
 
+class Coupon(models.Model):
+    couponType = models.ForeignKey(CouponType, on_delete=models.CASCADE)
+    couponValue = models.DecimalField(decimal_places=2, max_digits=3)
+    couponName = models.CharField(max_length=100)
+    couponDescription = models.CharField(max_length=250)
+
+class PropertyType(models.Model):
+    propertyTypeName = models.CharField(max_length=100)
+    propertyTypeDescription = models.CharField(max_length=512)
+
+# class Property(models.Model):
+#     propertyType = models.ForeignKey(PropertyType, on_delete=models.CASCADE)
+#     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+#     propertyAddress = models.CharField(max_length=100)
+#     propertyCreatedDate = models.DateField()
+#     propertyMarketPrice = models.DecimalField(decimal_places=2)
+#     propertyDescription = models.CharField(max_length=512)
+#     propertySqFt = models.IntegerField(min_value=0)
+#     propertyBedrooms = models.IntegerField(min_value=0)
+#     propertyBathrooms = models.IntegerField(min_value=0)
+
+# class Offer(models.Model):
+#     propertyBuilding = models.ManyToManyField(Property)
+#     offerAmount = models.DecimalField(decimal_places=2, max_digits=6)
+#     offerDate = models.DateTimeField(datetime.datetime.now())
+    #offerCounter
+    #offerDate
+    #userID
 
 class Neighborhood(models.Model):
     neighborhood_id = models.AutoField(primary_key=True)
     neighborhood_name = models.CharField(max_length=225)
     neighborhood_desc = models.TextField()
-
 
 class Property_Photo(models.Model):
     property_photo_id = models.AutoField(primary_key=True)
@@ -28,3 +59,4 @@ class Amenity(models.Model):
     amenity_id = models.AutoField(primary_key=True)
     amenity_name = models.CharField(max_length=225)
     amenity_desc = models.TextField()
+
