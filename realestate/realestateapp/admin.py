387aa75c6258b8172ef182f2a3c782a80758d74d
joesyoon <joesyoon@uw.edu>
2019-05-11 19:52:25 -0700

    Adding initial branches

--- realestate/realestateapp/admin.py
+++ realestate/realestateapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CouponType, Coupon, PropertyType
 
 # Register your models here.
+admin.site.register(CouponType)
+admin.site.register(Coupon)
+admin.site.register(PropertyType)
--- realestate/realestateapp/models.py
+++ realestate/realestateapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class CouponType(models.Model):
+    couponTypeName = models.CharField(max_length=30)
+    couponTypeDescription = models.CharField(max_length=250)
+
+class Coupon(models.Model):
+    couponType = models.ForeignKey(CouponType, on_delete=models.CASCADE)
+    couponValue = models.DecimalField(decimal_places=2, max_digits=3)
+    couponName = models.CharField(max_length=100)
+    couponDescription = models.CharField(max_length=250)
+
+class PropertyType(models.Model):
+    propertyTypeName = models.CharField(max_length=100)
+    propertyTypeDescription = models.CharField(max_length=512)
+
+# class Property(models.Model):
+#     propertyType = models.ForeignKey(PropertyType, on_delete=models.CASCADE)
+#     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+#     propertyAddress = models.CharField(max_length=100)
+#     propertyCreatedDate = models.DateField()
+#     propertyMarketPrice = models.DecimalField(decimal_places=2, max_digits=6)
+#     propertyDescription = models.CharField(max_length=512)
+#     propertySqFt = models.CharField(max_length=10)
+#     propertyBedrooms = models.CharField(max_length=10)
+#     propertyBathrooms = models.CharField(max_length=10)
+
+# class Offer(models.Model):
+#     propertyBuilding = models.ManyToManyField(Property)
+#     offerAmount = models.DecimalField(decimal_places=2, max_digits=6)
+#     offerDate = models.DateTimeField(datetime.datetime.now())
+    #offerCounter
+    #offerDate
+    #userID
