5542aba323cc88df4d84e9cf786888e66b5ec1d1
joesyoon <joesyoon@uw.edu>
2019-05-14 02:16:34 -0700

    Fixed syntax error

--- realestate/realestateapp/urls.py
+++ realestate/realestateapp/urls.py
@@ -4,11 +4,8 @@ from . import views
 app_name = 'realestateapp'
 urlpatterns = [
         path('coupons', views.coupons, name='coupons'),
-<<<<<<< HEAD
-        path('properties', views.properties, name='properties')
-=======
+        path('properties', views.properties, name='properties'),
         path('auth/register', views.register, name='account-create'),
         path('checkout', views.checkout, name="Checkout"),
         path('coupons/<coupon_id>', views.checkout, name="edit-certain-coupon"),
->>>>>>> master
 ]
