39f3c10e82d18758fe042bd33295c4412b9af982
Zubair Amjad <amjadz@uw.edu>
2019-05-14 01:43:22 -0700

    Working on coupon stuff

--- realestate/realestateapp/urls.py
+++ realestate/realestateapp/urls.py
@@ -5,5 +5,8 @@ app_name = 'realestateapp'
 urlpatterns = [
         path('coupons', views.coupons, name='coupons'),
         path('auth/register', views.register, name='account-create'),
-        path('checkout', views.checkout, name="Checkout")
+        path('checkout', views.checkout, name="Checkout"),
+        path('/coupons/<coupon_id>', views.checkout, name="edit-certain-coupon"),
+
+        
 ]
