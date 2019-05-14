86eef2acda452f4e9b43a976a09335d1dd78496e
joesyoon <joesyoon@uw.edu>
2019-05-14 00:30:07 -0700

    Updates urls and models

---
+++ realestate/realestateapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'realestateapp'
+urlpatterns = [
+        path('coupons', views.coupons, name='coupons'),
+]
--- realestate/realestateapp/views.py
+++ realestate/realestateapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Coupon, CouponType
 
 # Create your views here.
+def coupons(request):
+    if request.method == 'GET':
+        coupons = Coupon.objects.all()
+        couponsList = []
+        for coupon in coupons:
+            couponTemp = {
+                "couponName":coupon.couponName,
+                "couponValue":coupon.couponValue,
+                "couponDescription":coupon.couponDescription,
+                "couponTypeName":coupon.couponType.couponTypeName,
+                "couponTypeDescription":coupon.couponType.couponTypeDescription
+            }
+            couponsList.append(couponTemp)
+        return JsonResponse(couponsList, safe=False, status=200)
+    elif request.method == 'POST':
+    elif request.method == 'DELETE':
