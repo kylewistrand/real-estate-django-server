1bc1afe21e34a0c2566bb7083528c4cdff5063da
Zubair Amjad <amjadz@uw.edu>
2019-06-05 19:30:10 -0700

    Got stuff

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'realestateapp'
+    'realestateapp',
+    'pages'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'realestate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
